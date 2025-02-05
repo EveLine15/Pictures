1b86282e7391502fc4631d515414fcc92e2e58ce
EveLine15 <nnasta788@gmail.com>
2025-02-05 23:20:52 +0300

    add project

---
+++ script.js
@@ -0,0 +1,21 @@
+const body = document.querySelector('body');
+const holder = document.querySelector('.pictures-holder');
+
+localStorage.bgImage ? setBackgroundPicture(localStorage.bgImage): setBackgroundPicture("./images/image1.png");
+
+holder.addEventListener('click', (event) => {
+    if(event.target.matches("img")){
+
+        holder.querySelectorAll('img').forEach(element => {
+            element.classList.remove('img-chosen');
+        });
+        
+        setBackgroundPicture(event.target.getAttribute('src'));
+        event.target.classList.toggle('img-chosen');
+    } 
+});
+
+function setBackgroundPicture(item){
+    body.style.backgroundImage = `url(${item})`;
+    localStorage.bgImage = item;
+}
